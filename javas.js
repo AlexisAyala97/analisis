c0af3e7c87d332375213db2986eb20060e2a73ca
Ezio2220 <fjosue447@gmail.com>
2019-03-27 14:14:19 -0600

    agarra funciones de x² x³ y trigonometricas
    el unico problema esque debes avisar que no se puede poner solo x^2 o x^3 o cos(x) sen(x) tan(x) sino que si no llevan ningun numero almenos debes ponerles un 1 por ejemplo "2x+1x+3" o 2x^2+1tan(x) ;

--- javas.js
+++ javas.js
@@ -10,27 +10,43 @@ var criterio;
 var ax=0;
 var f2;
 var tab=document.getElementById("contenido");
+function evfn(cadena,valor){
+
+    var f3 = cadena.replace("x^3","*"+Math.pow(valor,3));
+    f3=f3.replace("x^2","*"+Math.pow(valor,2));
+    f3=f3.replace("cos(x)","*"+Math.cos(valor));
+    f3=f3.replace("sen(x)","*"+Math.sin(valor));
+    f3=f3.replace("tan(x)","*"+Math.tan(valor));
+    f3=f3.replace("x",'*'+valor);
+
+    return f3;
+}
 function calcular(){
     an=document.getElementById("an").value;
     bn=document.getElementById("bn").value;
     f=document.getElementById("fn").value;
     tol=parseFloat(Math.pow(10,-document.getElementById("tol").value));
     var acum=" ";
-    do{
     ax=(parseFloat(an)+parseFloat(bn))/2;
-    f2=f.replace("x^3","xxx");
-    f2=f2.replace("x^2","xx");
-    f2=f2.replace("x",'*'+ax);
-    fp=eval(f2);
+    n=1;
+    if(evfn(f,an)*evfn(f,bn)>0){
+        alert("NO HAY RAICEZ PARA esa funcion en ese intervalo!");
+    }else{
+    do{
+    ax=(parseFloat(an)+parseFloat(bn))/2;   
+    
+   
+    fp=eval(evfn(f,ax));
+    
     if(n==1){
         Er=0;
     }else{
-        Er=(parseFloat(ax)-parseFloat(Pn))/parseFloat(ax);
+        Er=Math.abs((parseFloat(ax)-parseFloat(Pn))/parseFloat(ax));
     }
     Pn=ax;
     
     tab=document.getElementById("contenido");
-    alert(f2);
+    
     acum+= "<tr> <th scope='row'>"+n+"</th>"+
     "<td>"+an+"</td><td>"+bn+"</td>"+
     "<td>"+ax+"</td><td>"+fp+"</td>"+
@@ -47,7 +63,8 @@ function calcular(){
         Er=999;
     }    
     n++;
-    }while(Er<tol);
-    
+    }while(Er>tol);
+    document.getElementById("respuesta").innerHTML="<center><h2>LA RAIZ ES: "+ax+"</h2>";
     tab.innerHTML=acum;
+    }
 }
